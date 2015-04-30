fa7de3626305a0d27a88b74ceb44781386d64148
chrisc <chrisc@caldwell.ws>
2015-04-30 10:11:50 -0700

    stuff

---
+++ pdfClear/src/pdfClear/ImportFileInterace.java
@@ -0,0 +1,10 @@
+package pdfClear;
+
+import java.util.List;
+
+
+public interface ImportFileInterace{
+	/**import single page .hocr file
+	 * @return list of word objects */
+	public List<Object> readHOCR(String pagePath);	
+}
