8886206a66e3f5effd9048bfbd945a11704588e6
chrisc <chrisc@caldwell.ws>
2015-05-05 06:43:13 -0700

    .

--- pdfClear/src/pdfClear/PostProcess.java
+++ pdfClear/src/pdfClear/PostProcess.java
@@ -7,6 +7,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PostProcess implements PostProcessInterface {
+	private int _outType=0; //type of files to output
+	private String _workFolder;
+	
+	public PostProcess(int outType, String workFolder){
+		_outType=outType;
+		_workFolder=workFolder;
+	}
 
+	@Override
+	public void outputFile(ArrayList<Word> wordList,
+			ArrayList<WordBlock> wordLayout, String baseName) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
--- pdfClear/src/pdfClear/PostProcessInterface.java
+++ pdfClear/src/pdfClear/PostProcessInterface.java
@@ -6,4 +6,8 @@ import java.util.List;
 
 
 public interface PostProcessInterface{
+	/*
+	 * takes wordList and wordLayout, constructs output file, cleans up temporary files
+	 */
+	public void outputFile(ArrayList<Word> wordList,ArrayList<WordBlock> wordLayout, String baseName);
 }
